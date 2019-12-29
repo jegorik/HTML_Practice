0f8449f9e3f23753da54af0abb42dd11480c5b06
jegorik <jegorik@aroundacloud.co.uk>
2019-12-29 14:50:48 +0200

    JS, DOM

--- DOM/js/index.js
+++ DOM/js/index.js
@@ -1,9 +1,11 @@
 function game() {
     var player1Score = Math.floor((Math.random() * 6) + 1);
     var player2Score = Math.floor((Math.random() * 6) + 1);
+    var diceImage1 = "images/" + player1Score + ".png";
+    var diceImage2 = "images/" + player2Score + ".png";
 
-    document.querySelector("#player1").innerHTML = "<img src=images/" + player1Score + ".png" + ">";
-    document.querySelector("#player2").innerHTML = "<img src=images/" + player2Score + ".png" + ">";
+    document.querySelector("#player1").innerHTML = "<img src=" + diceImage1 + ">";
+    document.querySelector("#player2").innerHTML = "<img src=" + diceImage2 + ">";
 
     if (player1Score > player2Score) {
         document.querySelector("#result").innerHTML = "&#x270C Player 1 win!";
@@ -14,4 +16,4 @@ function game() {
     }
 }
 
-document.onload(game());
+game();
